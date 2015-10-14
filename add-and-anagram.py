3a7ba435cddc112871b5d300e083e8f147d48b39
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2015-10-15 01:56:23 +0530

    Replace dictionary list with trie Replace set comparison with hashing

--- add-and-anagram.py
+++ add-and-anagram.py
@@ -1,10 +1,18 @@
+import marisa_trie
+
 with open('ospd.txt') as word_file:
 	dictionary = []
 	for line in word_file:
-		dictionary.append(line.strip())
+		dictionary.append(unicode(line.strip()))
 
 dictionary_by_length = dict()
 
+# A prefix trie of all words. Gives a lookup time of O(word_length).
+# Considering that the dictionary does not contain words longer than 10 letters
+# we can reasonably assume the time complexity to be O(1) for lookup
+trie = marisa_trie.Trie(dictionary)
+
+# Dictionary ordered by length to speed up retrieval of words of a specific length
 for i in range(1, 10):
 	dictionary_by_length[i] = []
 
@@ -13,37 +21,68 @@ for word in dictionary:
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+# Used for evaluating whether anagram checking by using a set or using a hash is faster
+# As expected, using the hash is faster
+def check_anagram_set(word_set, word2):
+	if word_set == set(word2):
+		return True
+	return False
+
+def check_anagram_hash(word_hash, word2):
+	test_hash = word_hash[:]
+
+	for letter in word2:
+		test_hash[ord(letter) - 97] -= 1
+
+	# If there are no non-zero entries in the test hash, return true
+	return len(filter(lambda a: a != 0, test_hash)) == 0
+
+# A depth-first lookup for valid word sequences
 def add_and_anagram(word, current_word_sequence, word_length):
-	if word not in dictionary_by_length[word_length] or word_length > 9:
+	if word_length > 9:
 		return
 
 	if len(current_word_sequence) > 5:
 		print current_word_sequence
 		return
-	letter_set = set(word)
-
+	
 	anagrams = []
 
+	# Hash of the letters of the given word
+	word_hash = [0]*26
+
+	for letter in word:
+		word_hash[ord(letter) - 97] += 1
+
+	# Testing if two words are anagrams by letter counting
 	for test_word in dictionary_by_length[word_length]:
-		if set(test_word) == letter_set:
+		test_hash = word_hash[:]
+
+		for letter in test_word:
+			test_hash[ord(letter) - 97] -= 1
+
+		# If there are no non-zero entries in the test hash, then append to dictionady
+		if len(filter(lambda a: a != 0, test_hash)) == 0:
 			anagrams.append(test_word)
 
 	for test_anagram in anagrams:
 		for letter in alphabet:
 			test_word = letter + test_anagram
-			add_and_anagram(test_word, current_word_sequence + [test_word], word_length + 1)
+			if test_word in trie:
+				add_and_anagram(test_word, current_word_sequence + [test_word], word_length + 1)
 
 			for i in range(1, word_length):
 				test_word = test_anagram[:i] + letter + test_anagram[i:]
-				add_and_anagram(test_word, current_word_sequence + [test_word], word_length + 1)
+				if test_word in trie:
+					add_and_anagram(test_word, current_word_sequence + [test_word], word_length + 1)
 
 			test_word = test_anagram + letter
-
-			add_and_anagram(test_word, current_word_sequence + [test_word], word_length + 1)
+			if test_word in trie:
+				add_and_anagram(test_word, current_word_sequence + [test_word], word_length + 1)
 
 def main():
 	print "Enter a word: "
-	word = raw_input()
+	word = unicode(raw_input())
 	add_and_anagram(word=word, current_word_sequence=[word], word_length=len(word))
 
 if __name__ == '__main__':
