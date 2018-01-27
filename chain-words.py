6168bb138fc56145a433b740525263456d0e5f37
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2018-01-28 01:17:28 +0530

    Fix formatting

--- chain-words.py
+++ chain-words.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 def find_chain_words(base_word, dictionary, chain_length):
     # Ideally for chain words, it's better to use six letter words
     word_pool = [word for word in dictionary if len(word) == 6 or len(word) == 7]
@@ -7,11 +5,9 @@ def find_chain_words(base_word, dictionary, chain_length):
     prefix = base_word[:chain_length] # get the first 3 letters of the word
 
     front_words = []
-    
     for word in word_pool:
         if word[-chain_length:] == prefix:
             front_words.append(word)
-    
     print(chain_length,"-chain front words for the given word are: ", front_words)
 
     suffix = base_word[-chain_length:]
@@ -20,7 +16,7 @@ def find_chain_words(base_word, dictionary, chain_length):
         if word[:chain_length] == suffix:
             back_words.append(word)
 
-    print(chain_length,"-chain back words for the given word are: ", back_words)
+    print(chain_length, "-chain back words for the given word are: ", back_words)
 
 if __name__ == "__main__":
     word = input("Please enter the word to find chains for: ")
@@ -28,5 +24,5 @@ if __name__ == "__main__":
     with open('ospd.txt') as file_reader:
         for line in file_reader:
             dictionary.append(line.strip())
-    find_chain_words(word, dictionary,3)
-    find_chain_words(word, dictionary,4)
+    find_chain_words(word, dictionary, 3)
+    find_chain_words(word, dictionary, 4)
